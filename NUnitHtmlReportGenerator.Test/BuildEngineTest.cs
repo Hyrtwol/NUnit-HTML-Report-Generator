d416f94185ed3daa1b737cb35ce6ae763bb1bbfe
Thomas la Cour <tlc@hyrtwol.dk>
2015-06-18 01:10:32 +0200

    converted the commandline tool into a msbuild task

---
+++ NUnitHtmlReportGenerator.Test/BuildEngineTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Build.BuildEngine;
+using Microsoft.Build.Framework;
+using NUnit.Framework;
+
+namespace Jatech.NUnit.Test
+{
+    public abstract class BuildEngineTest
+    {
+#pragma warning disable 0618
+
+        protected BuildEngineTest()
+        {
+            Verbosity = LoggerVerbosity.Normal;
+        }
+
+        protected Engine Engine { get; private set; }
+
+        protected string DefaultTargets { get; set; }
+        protected LoggerVerbosity Verbosity { get; set; }
+
+        [SetUp]
+        public virtual void SetUp()
+        {
+            //Console.WriteLine("Engine SetUp");
+            Engine = new Engine();
+            Engine.DefaultToolsVersion = "4.0";
+
+            WriteEngineVersions();
+            RegisterEngineLoggers();
+
+            /*
+            project = engine.CreateNewProject();
+            project.DefaultTargets = "Build";
+            var buildTarget = project.Targets.AddNewTarget("Build");
+            */
+        }
+
+        protected virtual void RegisterEngineLoggers()
+        {
+            var logger = new ConsoleLogger();
+            logger.Verbosity = Verbosity;
+            Engine.RegisterLogger(logger);
+        }
+
+        [Conditional("DEBUG")]
+        protected void WriteEngineVersions()
+        {
+            Debug.WriteLine("Engine.Version={0}", Engine.Version);
+            Debug.WriteLine("Engine.DefaultToolsVersion={0}", Engine.DefaultToolsVersion);
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+            //Console.WriteLine("Engine TearDown");
+            Engine.UnloadAllProjects();
+            Engine.UnregisterAllLoggers();
+            Engine.Shutdown();
+            Engine = null;
+        }
+
+        protected void AssertProjectBuild(Project project, bool expectedResult = true)
+        {           
+            Assert.AreEqual(expectedResult, project.Build(DefaultTargets), "MSBuild failed.");
+        }
+
+#pragma warning restore 0618
+    }
+}
---
+++ NUnitHtmlReportGenerator.Test/NUnitHtmlReportGeneratorTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.IO;
+using NUnit.Framework;
+
+namespace Jatech.NUnit.Test
+{
+    [TestFixture]
+    public class NUnitHtmlReportGeneratorTests : BuildEngineTest
+    {
+        [TestCase(@"TestData\ExampleResults.xml")]
+        public void GenerateHtmlFromNUnitTestResult(string inputFileName)
+        {
+            var project = Engine.CreateNewProject();
+
+            project.DefaultTargets = "Build";
+
+            //project.AddNewUsingTaskFromAssemblyName<NUnitHtmlReportGenerator>();
+            var taskType = typeof(NUnitHtmlReportGenerator);
+            project.AddNewUsingTaskFromAssemblyName(taskType.Name, taskType.Assembly.FullName);
+            Assert.IsNotNull(project.Targets);
+
+            var buildTarget = project.Targets.AddNewTarget("Build");
+
+            var task = buildTarget.AddNewTask("NUnitHtmlReportGenerator");
+            task.SetParameterValue("InputFileName", inputFileName);
+            task.SetParameterValue("OverwriteOutput", true.ToString());
+
+            Debug.WriteLine(project.Xml);
+            //Assert.IsTrue(project.Build(), "MSBuild failed.");
+            AssertProjectBuild(project);
+
+            Assert.IsTrue(File.Exists(@"TestData\ExampleResults.html"), "ExampleResults.html not found");
+        }
+    }
+}
